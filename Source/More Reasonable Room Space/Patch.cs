5b6d85e6726631e42d4c5cce88a1e2d6fb2f4ac9
Kagam11 <hiiragi.kagami@foxmail.com>
2025-06-14 20:00:01 +0800

    initial

---
+++ Source/More Reasonable Room Space/Patch.cs
@@ -0,0 +1,74 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Verse;
+
+namespace MoreReasonableRoomSpace
+{
+    [StaticConstructorOnStartup]
+    internal static class InitializeHarmony
+    {
+        static InitializeHarmony()
+        {
+            var harmony = new Harmony("kagami.MoreReasonableRoomSpace");
+            harmony.PatchAll();
+        }
+    }
+    [StaticConstructorOnStartup]
+    internal static class MrrsStartUp
+    {
+        static MrrsSettings settings;
+        static MrrsStartUp()
+        {
+            settings = LoadedModManager.GetMod<MrrsMod>().GetSettings<MrrsSettings>();
+            var scoreStages = DefDatabase<RoomStatDef>.GetNamed("Space").scoreStages;
+            foreach (var stage in scoreStages)
+            {
+                //Log.Warning($"Stage: {stage.label} - MinScore: {stage.minScore}");
+                switch (stage.untranslatedLabel)
+                {
+                    case "rather tight":
+                        stage.minScore = settings.Rathertight; break;
+                    case "averaged-sized":
+                        stage.minScore = settings.AverageSized; break;
+                    case "somewhat spacious":
+                        stage.minScore = settings.SomewhatSpacious; break;
+                    case "quite spacious":
+                        stage.minScore = settings.QuiteSpacious; break;
+                    case "very spacious":
+                        stage.minScore = settings.VerySpacious; break;
+                    case "extremely spacious":
+                        stage.minScore = settings.ExtremelySpacious; break;
+                    default:
+                        break;
+                }
+                //Log.Warning($"Updated Stage: {stage.label} - MinScore: {stage.minScore}");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(RoomStatWorker_Space), nameof(RoomStatWorker_Space.GetScore))]
+    public static class Patch
+    {
+        public static bool Prefix(Room room, ref float __result)
+        {
+            if (room.PsychologicallyOutdoors)
+            {
+                __result = 100f;
+                return false;
+            }
+            float totalCellCount = room.Cells.Count();
+            int standableCellCount = 0;
+            foreach (var cell in room.Cells)
+            {
+                if (cell.Standable(room.Map)) standableCellCount++;
+            }
+            var spacefullness = (100 * (standableCellCount) / totalCellCount);
+            var penalty = Math.Min(totalCellCount / MrrsMod.settings.PenaltyMinSize, 1);
+            __result = (float)Math.Round(penalty * spacefullness, 2);
+            return false;
+        }
+    }
+}
---
+++ Source/More Reasonable Room Space/Settings.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace MoreReasonableRoomSpace
+{
+    public class MrrsSettings : ModSettings
+    {
+        public float Rathertight = 15f;
+        public float AverageSized = 30f;
+        public float SomewhatSpacious = 45f;
+        public float QuiteSpacious = 60f;
+        public float VerySpacious = 75f;
+        public float ExtremelySpacious = 85f;
+
+        public int PenaltyMinSize = 25;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref Rathertight, nameof(Rathertight), 15f);
+            Scribe_Values.Look(ref AverageSized, nameof(AverageSized), 30f);
+            Scribe_Values.Look(ref SomewhatSpacious, nameof(SomewhatSpacious), 45f);
+            Scribe_Values.Look(ref QuiteSpacious, nameof(QuiteSpacious), 60f);
+            Scribe_Values.Look(ref VerySpacious, nameof(VerySpacious), 75f);
+            Scribe_Values.Look(ref ExtremelySpacious, nameof(ExtremelySpacious), 85f);
+
+            Scribe_Values.Look(ref PenaltyMinSize, nameof(PenaltyMinSize), 25);
+            base.ExposeData();
+        }
+    }
+    public class MrrsMod : Mod
+    {
+        public static MrrsSettings settings;
+
+        public MrrsMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<MrrsSettings>();
+        }
+        public override string SettingsCategory() => "MoreReasnonableRoomSpace".Translate();
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+            listing.Label("SpacefullnessExplain".Translate());
+            listing.GapLine();
+            listing.Label("SpacefullnessLabel".Translate());
+
+            settings.Rathertight = listing.FloatEntry(nameof(settings.Rathertight).Translate(), settings.Rathertight, 0f, 100f);
+            settings.AverageSized = listing.FloatEntry(nameof(settings.AverageSized).Translate(), settings.AverageSized, 0f, 100f);
+            settings.SomewhatSpacious = listing.FloatEntry(nameof(settings.SomewhatSpacious).Translate(), settings.SomewhatSpacious, 0f, 100f);
+            settings.QuiteSpacious = listing.FloatEntry(nameof(settings.QuiteSpacious).Translate(), settings.QuiteSpacious, 0f, 100f);
+            settings.VerySpacious = listing.FloatEntry(nameof(settings.VerySpacious).Translate(), settings.VerySpacious, 0f, 100f);
+            settings.ExtremelySpacious = listing.FloatEntry(nameof(settings.ExtremelySpacious).Translate(), settings.ExtremelySpacious, 0f, 100f);
+
+            listing.GapLine();
+            listing.Label("PenaltyExplain".Translate());
+            var penaltyBuffer = settings.PenaltyMinSize.ToString();
+            listing.IntEntry(ref settings.PenaltyMinSize, ref penaltyBuffer);
+            listing.Label("PenaltyFormula".Translate());
+
+            listing.Label("Comment".Translate());
+            listing.GapLine();
+
+            bool buttonPressed = listing.ButtonText("Reset".Translate());
+
+            listing.End();
+            base.DoSettingsWindowContents(inRect);
+            if (buttonPressed)
+            {
+                settings.Rathertight = 15f;
+                settings.AverageSized = 30f;
+                settings.SomewhatSpacious = 45f;
+                settings.QuiteSpacious = 60f;
+                settings.VerySpacious = 75f;
+                settings.ExtremelySpacious = 85f;
+                settings.PenaltyMinSize = 25;
+            }
+        }
+    }
+    public static class Extensions
+    {
+        public static float FloatEntry(this Listing_Standard ls, string label, float val, float min, float max)
+        {
+            var buffer = val.ToString("F2");
+            ls.TextFieldNumericLabeled(label, ref val, ref buffer, min, max);
+            return val;
+        }
+    }
+}
